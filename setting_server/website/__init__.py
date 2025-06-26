73dd83174c877fa50ebb7dc27168a2c6810ab471
GlobusShwobus <fjodorzaro@gmail.com>
2025-06-26 12:37:38 +0300

    abcd

--- setting_server/website/__init__.py
+++ setting_server/website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY']= 'boblazarisanalien'
+    app.config['SECRET_KEY']= 'boblazarisanalien'#throwaway, doesn't matter
 
     from .views import views
     from .auth import auth
