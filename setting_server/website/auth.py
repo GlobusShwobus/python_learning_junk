ba41e44d71ebf10f8f186a64bab451a2ea1f97da
GlobusShwobus <fjodorzaro@gmail.com>
2025-06-28 14:02:47 +0300

    flash message, jinja bullshit and i think cleaner html ikd

--- setting_server/website/auth.py
+++ setting_server/website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,29 @@ def login():
 def logout():
     return render_template("home.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods = ['GET', 'POST'])
 def sign_up():
+    if request.method=='POST':
+        email=request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email)<4:
+            flash('Email must be greater than 4 chars', category='error')
+            pass
+        elif len(firstName)<2:
+            flash('first name must be greater than 2 chars', category='error')
+            pass
+        elif password1!=password2:
+            flash('passwords dont match', category='error')
+            pass
+        elif len(password1)<7:
+            flash('password too shlong', category='error')
+            pass
+        else:
+            #add user
+            flash('account created', category='success')
+            pass
+
     return render_template("signup.html")
